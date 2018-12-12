d5b01a409be67798ecb758936cba9b42f4eb40b3
Jonas Skovmand <skovmand@ustwo.com>
2018-12-12 11:23:15 +0100

    Index components for snazzier imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Meta from "./components/Meta";
-import Player from "./components/Player";
-import Stats from "./components/Stats";
-import Stream from "./components/Stream";
+import { Header, Meta, Player, Stats, Stream } from "./components";
 import { Provider } from "./Store";
 
 class App extends Component {
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header";
+export { default as Meta } from "./Meta";
+export { default as Player } from "./Player";
+export { default as Stats } from "./Stats";
+export { default as Stream } from "./Stream";
